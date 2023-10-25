670cc3316eb80c3408670de4b3faf85a3a8239d8
jeansenecal <jeansenecal02@gmail.com>
2023-10-24 22:25:51 -0400

    more html

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import './index.css';
 import Navigation from './Navigation';
+import SetupSelector from './SetupSelector';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <p>
-        This is a test
-      </p>
-      <p>
-        This is a test
-      </p>
-      <button className="btn rounded-full">Button</button>
+      <SetupSelector />
     </div>
   );
 }
---
+++ frontend/src/SetupSelector.tsx
@@ -0,0 +1,11 @@
+
+export default function Navigation() {
+    return ( 
+        <>
+        <select className="select select-bordered w-full max-w-xs">
+            <option disabled selected>Who shot first?</option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+        </select>
+        </>
+    )}
