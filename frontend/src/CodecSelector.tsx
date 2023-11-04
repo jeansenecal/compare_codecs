7317e158bc3a1c3ce6fd14c9db68a539366edf17
jeansenecal <jeansenecal02@gmail.com>
2023-11-04 15:13:15 -0400

    sdf

--- frontend/src/CodecSelector.tsx
+++ frontend/src/CodecSelector.tsx
@@ -1,5 +1,15 @@
 export default function CodecSelector() {
     return ( 
-        <>
-        </>
+        <div className="flex flex-row flex-wrap justify-center mt-20">
+            <select className="select select-bordered w-full max-w-xs mr-10">
+                <option disabled selected>Codec 1</option>
+                <option>Han Solo</option>
+                <option>Greedo</option>
+            </select>
+            <select className="select select-bordered w-full max-w-xs">
+                <option disabled selected>Codec 2</option>
+                <option>Han Solo</option>
+                <option>Greedo</option>
+            </select>
+        </div>
     )}
