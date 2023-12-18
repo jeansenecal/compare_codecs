0d3075636b099de630b08fdbfdb79c3c1e489ac2
jeansenecal <jeansenecal02@gmail.com>
2023-12-17 23:06:18 -0500

    working on test page

--- frontend/src/ABX.tsx
+++ frontend/src/ABX.tsx
@@ -5,7 +5,10 @@ export default function ABX (){
         <div>
             <Navigation />
             {/*A*/}
-        
+            <div className="flex flex-col items-center gap-x-48 m-12">
+            <h1>Play each of the samples and try to identify whether sample X is the same as sample A or B</h1>
+            <h2>Now playing: Song</h2>
+            </div>  
             <div className="flex flex-row justify-center gap-x-48 m-12">
                 <div className="card w-96 bg-primary text-neutral-content">
                     <div className="card-body items-center text-center">
@@ -43,11 +46,11 @@ export default function ABX (){
                 </div>
             </div>
 
-            <div className="flex flex-row justify-center">
-                {/*C*/}
+            <div className="flex flex-col items-center">
+                {/*X*/}
                 <div className="card w-96 bg-neutral text-neutral-content">
                     <div className="card-body items-center text-center">
-                        <h2 className="card-title text-9xl">C</h2>
+                        <h2 className="card-title text-9xl">X</h2>
                         <div className="card-actions justify-end">
                             <label className="swap">
             
@@ -59,7 +62,15 @@ export default function ABX (){
                         </div>
                     </div>
                 </div>
-                </div>
+                <h1 className="mt-10">Which sample sounds most similar to sample X? 
+                    <button className="btn btn-outline btn-info mx-3">A</button>
+                    <button className="btn btn-outline btn-info mx-3">B</button>
+                    <button className="btn">Continue
+                    <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M6 12H18M18 12L13 7M18 12L13 17" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+                    </button>
+                </h1>
+            </div>
+            
         </div>
     )
 
