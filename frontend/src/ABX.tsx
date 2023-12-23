b461281b0114a2fbda3a56423194c96bd5ae4d82
jeansenecal <jeansenecal02@gmail.com>
2023-12-22 22:08:25 -0500

    idk

--- frontend/src/ABX.tsx
+++ frontend/src/ABX.tsx
@@ -9,7 +9,8 @@ export default function ABX (){
 
     const [isButtonASelected, setisButtonASelected] = useState(false);
     const [isButtonBSelected, setisButtonBSelected] = useState(false);
-
+    const [playerCurrentlyPlaying, setPlayerCurrentlyPlaying] = useState("");
+    console.log(playerCurrentlyPlaying)
     const toggleButtons = (event: React.MouseEvent<HTMLButtonElement>, button: string) => {
         if (button === buttonA) {
             setisButtonASelected(!isButtonASelected);
@@ -33,40 +34,32 @@ export default function ABX (){
             <h2>Now playing: Song</h2>
             </div>  
             <div className="flex flex-row justify-center gap-x-48 m-12">
-                <Player url="" name='A'/>
+                <Player 
+                    songUrl="https://res.cloudinary.com/dj6k5whza/video/upload/v1703205606/jpnow1wq6ku8bdruga6t.flac"
+                    name='A'
+                    colour="primary"
+                    checked={playerCurrentlyPlaying === "A"}
+                    setPlayerCurrentlyPlaying={setPlayerCurrentlyPlaying}
+                />
                 {/*B*/}
-                <div className="card w-96 bg-primary text-neutral-content">
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title text-9xl">B</h2>
-                        <div className="card-actions justify-end">
-                            <label className="swap">
-        
-                            <input type="checkbox" />
-                            <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"/></svg>
-                            <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"/></svg>
-                            
-                            </label>
-                        </div>
-                    </div>
-                </div>
+                <Player 
+                    songUrl="https://res.cloudinary.com/dj6k5whza/video/upload/v1703205606/jpnow1wq6ku8bdruga6t.flac"
+                    name='B'
+                    colour="primary"
+                    checked={playerCurrentlyPlaying === "B"}
+                    setPlayerCurrentlyPlaying={setPlayerCurrentlyPlaying}
+                />
             </div>
 
             <div className="flex flex-col items-center">
                 {/*X*/}
-                <div className="card w-96 bg-neutral text-neutral-content">
-                    <div className="card-body items-center text-center">
-                        <h2 className="card-title text-9xl">X</h2>
-                        <div className="card-actions justify-end">
-                            <label className="swap">
-            
-                            <input type="checkbox" />
-                            <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"/></svg>
-                            <svg className="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M3,9H7L12,4V20L7,15H3V9M16.59,12L14,9.41L15.41,8L18,10.59L20.59,8L22,9.41L19.41,12L22,14.59L20.59,16L18,13.41L15.41,16L14,14.59L16.59,12Z"/></svg>
-                            
-                            </label>
-                        </div>
-                    </div>
-                </div>
+                <Player 
+                    songUrl="https://res.cloudinary.com/dj6k5whza/video/upload/v1703205606/jpnow1wq6ku8bdruga6t.flac"
+                    name='X'
+                    colour="neutral"
+                    checked={playerCurrentlyPlaying === "X"}
+                    setPlayerCurrentlyPlaying={setPlayerCurrentlyPlaying}
+                />
                 <h1 className="mt-10">Which sample sounds most similar to sample X? 
                     <button className={isButtonASelected ? "btn btn-info mx-3" : "btn btn-outline btn-info mx-3"} onClick={(event) => toggleButtons(event, buttonA)}>A</button>
                     <button className={isButtonBSelected ? "btn btn-info mx-3" : "btn btn-outline btn-info mx-3"} onClick={(event) => toggleButtons(event, buttonB)}>B</button>
--- frontend/src/Player.tsx
+++ frontend/src/Player.tsx
@@ -1,72 +1,48 @@
 import { Howl, Howler } from "howler";
-import { useEffect } from "react";
+import { useRef, useState } from "react";
 
 interface PlayerProps {
   songUrl: string;
   name: string;
+  colour: string;
+  checked: boolean;
+  setPlayerCurrentlyPlaying: React.Dispatch<React.SetStateAction<string>>
 }
 
-export default function Player({ songUrl, name }: PlayerProps) {
+export default function Player({ songUrl, name, colour, checked, setPlayerCurrentlyPlaying }: PlayerProps) {
 
-  let sound: Howl;
-
+  //Every time the component is rendered a new Howl instance is created. I need to use useRef to keep the instance referenced.
+  const soundRef: React.MutableRefObject<Howl> = useRef(new Howl({
+    src: [songUrl],
+    html5: true
+  }));
+  const sound: Howl = soundRef.current;
   const [playing, setPlaying] = useState(false);
-
+  const cardClassName: string = colour === "neutral" ? "card w-96 bg-neutral text-neutral-content" : "card w-96 bg-primary text-neutral-content";
+  const [isChecked, setIsChecked] = useState(checked);
+  console.log(`${name} is currently checked ${checked}`)
   const toggle = () => {
     setPlaying(!playing);
     if (playing) {
       sound.pause();
+      setIsChecked(false);
     } else {
+      Howler.stop();
       sound.play();
+      setIsChecked(true);
+      setPlayerCurrentlyPlaying(name);
     }
   }
 
-  // const stop = () => {
-  //   setPlaying(false);
-  //   sound.stop();
-  // }
-
-  // const play = () => {
-  //   setPlaying(true);
-  //   sound.play();
-  // }
-
-  // const pause = () => {
-  //   setPlaying(false);
-  //   sound.pause();
-  // }
-
-  // const seek = (time: number) => {
-  //   sound.seek(time);
-  // }
-
-  // const duration = () => {
-  //   return sound.duration();
-  // }
-
-  // const position = () => {
-  //   return sound.seek();
-  // }
-
-  // const volume = (vol: number) => {
-
-  useEffect(() => {
-    sound = new Howl({
-      src: [songUrl],
-      html5: true
-    });
-
-  });
-
   return ( 
-    <div className="card w-96 bg-primary text-neutral-content">
+    <div className={cardClassName}>
       <div className="card-body items-center text-center">
           <h2 className="card-title text-9xl">{name}</h2>
           <div className="card-actions justify-end">
               <label className="swap">
       
               {/* this hidden checkbox controls the state */}
-              <input type="checkbox" />
+              {isChecked ? <input type="checkbox" checked onChange={toggle}/> : <input type="checkbox" onChange={toggle}/>}
               
               {/* volume on icon */}
               <svg className="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M14,3.23V5.29C16.89,6.15 19,8.83 19,12C19,15.17 16.89,17.84 14,18.7V20.77C18,19.86 21,16.28 21,12C21,7.72 18,4.14 14,3.23M16.5,12C16.5,10.23 15.5,8.71 14,7.97V16C15.5,15.29 16.5,13.76 16.5,12M3,9V15H7L12,20V4L7,9H3Z"/></svg>
